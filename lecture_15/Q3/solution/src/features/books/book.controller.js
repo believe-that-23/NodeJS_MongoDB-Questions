69721ff80cd36ccd11ca9b95121147551b204250
believe-that-23 <pandey.arnav2399@gmail.com>
2023-09-21 13:22:20 +0530

    correction made in solution files

--- lecture_15/Q3/solution/src/features/books/book.controller.js
+++ lecture_15/Q3/solution/src/features/books/book.controller.js
@@ -1,95 +1,93 @@
 import BookRepository from "./book.repository.js";
 
 export default class BookController {
-    constructor() {
-        this.bookRepository = new BookRepository();
+  constructor() {
+    this.bookRepository = new BookRepository();
+  }
+
+  createBook = async (req, res) => {
+    const { title, author, genre, copies, availableCopies } = req.body;
+    try {
+      const bookData = {
+        title,
+        author,
+        genre,
+        copies,
+        availableCopies,
+      };
+      await this.bookRepository.createBook(bookData);
+      res.status(201).json(bookData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: "Failed to create a new book" });
     }
+  };
 
-    createBook = async (req, res) => {
+  getOne = async (req, res) => {
+    const { bookId } = req.params;
+    console.log(bookId);
 
-        const { title, author, genre, copies, availableCopies } = req.body;
-        try {
-            const bookData = {
-                title,
-                author,
-                genre,
-                copies,
-                availableCopies
-            };
-            await this.bookRepository.createBook(bookData)
-            res.status(201).json(bookData);
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({ error: 'Failed to create a new book' });
-        }
-
+    try {
+      const book = await this.bookRepository.getOne(bookId);
+      if (!book) {
+        res.status(404).send("book  not found.");
+      } else {
+        res.status(200).send(book);
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: "Failed to find book" });
     }
+  };
 
-    getOne = async (req, res) => {
-        const { bookId } = req.params;
-        console.log(bookId);
+  listBooksByGenre = async (req, res) => {
+    const { genre } = req.params;
 
-        try {
-            const book = await this.bookRepository.getOne(bookId);
-            if (!book) {
-                res.status(404).send("book  not found.");
-            } else {
-                res.status(200).send(book);
-            }
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({ error: 'Failed to find book' });
-        }
+    try {
+      const book = await this.bookRepository.listBooksByGenre(genre);
+      if (!book) {
+        res.status(404).send("book not found!!");
+      } else {
+        res.status(200).send(book);
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: "Can't filter by Genre" });
     }
+  };
 
-    listBookByGenre = async (req, res) => {
-        const { genre } = req.params;
-
-        try {
-            const book = await this.bookRepository.listBooksByGenre(genre);
-            if (!book) {
-                res.status(404).send("book not found!!");
-            } else {
-                res.status(200).send(book);
-            }
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({ error: "Can't filter by Genre" });
-        }
+  updateBookAvailability = async (req, res) => {
+    const { bookId } = req.params;
+    const { quantity } = req.body;
+    // console.log(bookId + " " + quantity);
+    try {
+      const updatedBook = await this.bookRepository.updateBookAvailability(
+        bookId,
+        quantity
+      );
+      if (!updatedBook) {
+        res.status(404).json("book not found!!");
+      } else {
+        res.status(200).json(updatedBook);
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: "Could not update quantity" });
     }
+  };
 
-    updateBookAvailability = async (req, res) => {
-        const { bookId } = req.params;
-        const { quantity } = req.body;
-        // console.log(bookId + " " + quantity);
-        try {
-            const updatedBook = await this.bookRepository.updateBookAvailability(bookId, quantity);
-            if (!updatedBook) {
-                res.status(404).json("book not found!!");
-            } else {
-                res.status(200).json(updatedBook);
-            }
+  deleteBook = async (req, res) => {
+    const { bookId } = req.params;
 
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({ error: "Could not update quantity" })
-        }
+    try {
+      const deletedBook = await this.bookRepository.deleteBookById(bookId);
+      if (!deletedBook) {
+        return res.status(404).json({ message: "Book not found" });
+      }
+      return res.status(200).json({ message: "Book deleted" });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Failed to delete book" });
     }
-
-    deleteBook = async (req, res) => {
-        const { bookId } = req.params;
-
-        try {
-            const deletedBook = await this.bookRepository.deleteBookById(bookId);
-            if (!deletedBook) {
-                return res.status(404).json({ message: 'Book not found' });
-            }
-            return res.status(200).json({ message: "Book deleted" });
-
-        } catch (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Failed to delete book' });
-        }
-    }
-
+  };
 }
